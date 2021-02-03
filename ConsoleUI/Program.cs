b79fc0748b600bf5f5f1c9202d3de6f98f6b8cf3
MeltemAlbayrakk <albyrk.meltem@gmail.com>
2021-02-03 23:47:22 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        //S"O"LID
+        //Open Closed Principle: Yaptığın yazılıma yeni özellik ekliyorsan mevcut koduna dokunamazsın
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+            
+        }
+    }
+}
